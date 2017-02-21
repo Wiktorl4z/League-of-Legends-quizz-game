f799c32f03dd29c5fc857f5e4f99675f63866991
Wiktor <wiktorkalinowski@gmail.com>
2017-02-21 22:11:15 +0100

    add Question

---
+++ app/src/main/java/com/example/l4z/quizapp/Question.java
@@ -0,0 +1,32 @@
+package com.example.l4z.quizapp;
+
+public class Question {
+
+    private String question;
+    private String image;
+    private String answer;
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+}
