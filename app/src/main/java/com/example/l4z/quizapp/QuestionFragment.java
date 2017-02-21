721055dd76b25c05850b6ada7422e34b9a4200f0
Wiktor <wiktorkalinowski@gmail.com>
2017-02-22 00:21:33 +0100

    onClick fixed

--- app/src/main/java/com/example/l4z/quizapp/QuestionFragment.java
+++ app/src/main/java/com/example/l4z/quizapp/QuestionFragment.java
@@ -4,7 +4,9 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.TextView;
 
@@ -13,7 +15,7 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class QuestionFragment extends Fragment {
 
-    private int points;
+    private static int points;
     private AnswerType answer = AnswerType.NO_ANSWER;
     private AnswerType correctAnswer;
     public static final String QUESTION_OBJECT = "question";
@@ -24,24 +26,48 @@ public class QuestionFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         correctAnswer=AnswerType.ANSWER_1;
         View rootView = inflater.inflate(R.layout.fragment_question, container, false);
+        rootView.findViewById(R.id.buttonAnswer).setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                checkTheAnswer(view);
+            }
+        });
+        rootView.findViewById(R.id.answer1).setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                answerButtonClick(view);
+            }
+        });
+        rootView.findViewById(R.id.answer2).setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                answerButtonClick(view);
+            }
+        });
+        rootView.findViewById(R.id.answer3).setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                answerButtonClick(view);
+            }
+        });
         Bundle args = getArguments();
         ((TextView) rootView.findViewById(R.id.questionID)).setText(args.getString(QUESTION_OBJECT));
         int rnd = random.nextInt(3)+1;
         switch(rnd){
             case 1:
-            ((RadioButton) rootView.findViewById(R.id.asnwer1)).setText(args.getString(CORRECT_ANSWER_OBJECT));
+            ((RadioButton) rootView.findViewById(R.id.answer1)).setText(args.getString(CORRECT_ANSWER_OBJECT));
                 correctAnswer=AnswerType.ANSWER_1;
                 break;
             case 2:
-                ((RadioButton) rootView.findViewById(R.id.asnwer2)).setText(args.getString(CORRECT_ANSWER_OBJECT));
+                ((RadioButton) rootView.findViewById(R.id.answer2)).setText(args.getString(CORRECT_ANSWER_OBJECT));
                 correctAnswer=AnswerType.ANSWER_2;
                 break;
             case 3:
-                ((RadioButton) rootView.findViewById(R.id.asnwer3)).setText(args.getString(CORRECT_ANSWER_OBJECT));
+                ((RadioButton) rootView.findViewById(R.id.answer3)).setText(args.getString(CORRECT_ANSWER_OBJECT));
                 correctAnswer=AnswerType.ANSWER_3;
                 break;
             default:
-                ((RadioButton) rootView.findViewById(R.id.asnwer1)).setText(args.getString(CORRECT_ANSWER_OBJECT));
+                ((RadioButton) rootView.findViewById(R.id.answer1)).setText(args.getString(CORRECT_ANSWER_OBJECT));
                 correctAnswer=AnswerType.ANSWER_1;
         }
         return rootView;
